d2a03c800de6d504fb423c07236bf3d5f8aa650b
Christian Rivero <42509107+christianrivero@users.noreply.github.com>
2020-05-04 01:32:56 -0500

    Update bash_aliases.bash

--- bash_aliases.bash
+++ bash_aliases.bash
@@ -5,7 +5,7 @@ alias date='date +"%r" && date +"%d-%b-%Y"'
 alias clean-ram='free -h && sudo sync && sudo sysctl -w vm.drop_caches=3 && free -h'
 alias cat='ccat'
 alias git-timeout="git config --global credential.helper 'cache --timeout=36000'" #Duration: 10 hours
-alias space='du -sh'
+alias size='du -sh'
 alias extract='dtrx' #Install dtrx, previously. Works for tar,zip,cpio,deb,rpm,gem,7z,cab,lzh,rar,gz,bz2,lzma,xz
 alias battery='upower -i $(upower -e | grep BAT) | grep --color=never -E "state|to\ full|to\ empty|percentage"'
 alias cls='tput reset'
